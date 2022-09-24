1504a5e771cac4595fe9d04e2abb9b33ef7a85bd
Swarnadeep Sarkar <swarnadeep.click@gmail.com>
2022-09-24 15:43:38 +0530

    Added event.js, added enemies

---
+++ event.js
@@ -0,0 +1,19 @@
+let firing;
+
+canvas.addEventListener("mousedown", () => {
+	bullet.fire();
+	firing = setInterval(bullet.fire, 100);
+});
+canvas.addEventListener("mousemove", player.moveWithMouse);
+canvas.addEventListener("mouseup", () => {
+	clearInterval(firing);
+});
+
+canvas.addEventListener("touchstart", () => {
+	bullet.fire();
+	firing = setInterval(bullet.fire, 100);
+});
+canvas.addEventListener("touchmove", player.moveWithTouch);
+canvas.addEventListener("touchend", () => {
+	clearInterval(firing);
+});
