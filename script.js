f52540f6f6e58c81898c75b161f281fb659ecdf8
Swarnadeep Sarkar <swarnadeep.click@gmail.com>
2022-09-27 17:29:45 +0530

    Enemies will die when a bullet hits it.

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const game = {
-	clearCanvas() {
+	clear() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	},
+
+	draw() {
+		player.draw();
+		bullet.draw();
+		enemy.draw();
 	}
 };
 
@@ -17,73 +23,95 @@ const player = {
 		ctx.fillRect(player.x, player.y, player.width, player.height);
 	},
 
+	restrictCanvas() {
+		if(player.x < 0) {
+			player.x = 0;
+		}
+
+		if((player.x + player.width) > canvas.width) {
+			player.x = canvas.width - player.width;
+		}
+	},
+
 	moveWithMouse(e) {
 		player.x = e.clientX - (player.width / 2);
+		player.restrictCanvas();
 	},
 
 	moveWithTouch(e) {
 		player.x = e.touches[0].clientX - (player.width / 2);
+		player.restrictCanvas();
 	}
 };
 
 const bullet = {
-	x: [],
-	y: [],
+	arr: [],
 	width: 10,
 	height: player.height,
 
 	draw() {
-		for(let i = 0; i < bullet.y.length; i++) {
-			ctx.fillRect(bullet.x[i], bullet.y[i], bullet.width, bullet.height);
+		for(let i = 0; i < bullet.arr.length; i++) {
+			ctx.fillRect(bullet.arr[i].x, bullet.arr[i].y, bullet.width, bullet.height);
 		}
 	},
 
 	fire() {
-		bullet.x.push(player.x + bullet.width);
-		bullet.y.push(player.y - bullet.height);
+		bullet.arr.push({x: player.x + bullet.width, y: player.y});
 	},
 
 	move() {
 		// if no more bullets in array then exit function
-		if(bullet.y.length === 0) {
+		if(bullet.arr.length === 0) {
 			return;
 		}
 
-		for(let i = 0; i < bullet.y.length; i++) {
-			bullet.y[i] -= 10;
+		for(let i = 0; i < bullet.arr.length; i++) {
+			bullet.arr[i].y -= 10;
 		}
 
-		if((bullet.y[0] + bullet.height) <= 0) {
-			bullet.delete();
+		if((bullet.arr[0].y + bullet.height) < 0) {
+			bullet.delete(0);
 		}
 	},
 
-	delete() {
-		// if no more bullets in array then exit function
-		if(bullet.y.length === 0) {
-			return;
+	delete(bulletIndex) {
+		if(bulletIndex === 0) {
+			bullet.arr.shift();
+		}
+		else {
+			for(let i = bulletIndex; i < bullet.arr.length - 1; i++) {
+				bullet.arr[i].x = bullet.arr[i + 1].x;
+				bullet.arr[i].y = bullet.arr[i + 1].y;
+			}
+
+			bullet.arr.pop();
+		}
+	},
+
+	touchesEnemy(bulletIndex, enemyIndex) {
+		if( (bullet.arr[bulletIndex].y <= (enemy.arr[enemyIndex].y + enemy.height)) && ((enemy.arr[enemyIndex].y + enemy.height) < player.y) ) {
+			if( (bullet.arr[bulletIndex].x <= (enemy.arr[enemyIndex].x + enemy.width)) && ((bullet.arr[bulletIndex].x + bullet.width) >= enemy.arr[enemyIndex].x) ) {
+				return true;
+			}
 		}
 
-		bullet.x.shift();
-		bullet.y.shift();
+		return false;
 	}
 };
 
 const enemy = {
-	x: [],
-	y: [],
+	arr: [],
 	width: player.width,
 	height: player.height,
 
 	draw() {
-		for(let i = 0; i < enemy.y.length; i++) {
-			ctx.fillRect(enemy.x[i], enemy.y[i], enemy.width, enemy.height);
+		for(let i = 0; i < enemy.arr.length; i++) {
+			ctx.fillRect(enemy.arr[i].x, enemy.arr[i].y, enemy.width, enemy.height);
 		}
 	},
 
 	create() {
-		enemy.x.push(randomRange(0, canvas.width - enemy.width));
-		enemy.y.push(-enemy.height);
+		enemy.arr.push({x: randomRange(0, canvas.width - enemy.width), y: -enemy.height});
 
 		setTimeout(() => {
 			requestAnimationFrame(enemy.create);
@@ -92,27 +120,31 @@ const enemy = {
 
 	move() {
 		// if no more enemies left in array then exit function
-		if(enemy.y.length === 0) {
+		if(enemy.arr.length === 0) {
 			return;
 		}
 
-		for(let i = 0; i < enemy.x.length; i++) {
-			enemy.y[i]++;
+		for(let i = 0; i < enemy.arr.length; i++) {
+			enemy.arr[i].y++;
 		}
 
-		if(enemy.y[0] >= canvas.height) {
-			enemy.delete();
+		if(enemy.arr[0].y > canvas.height) {
+			enemy.delete(0);
 		}
 	},
 
-	delete() {
-		// if no more enemies left in array then exit function
-		if(enemy.y.length === 0) {
-			return;
+	delete(enemyIndex) {
+		if(enemyIndex === 0) {
+			enemy.arr.shift();
 		}
+		else {
+			for(let i = enemyIndex; i < enemy.arr.length - 1; i++) {
+				enemy.arr[i].x = enemy.arr[i + 1].x;
+				enemy.arr[i].y = enemy.arr[i + 1].y;
+			}
 
-		enemy.x.shift();
-		enemy.y.shift();
+			enemy.arr.pop();
+		}
 	}
 };
 
@@ -121,14 +153,21 @@ function randomRange(min, max) {
 }
 
 function main() {
-	game.clearCanvas();
-
-	player.draw();
-	bullet.draw();
-	enemy.draw();
+	game.clear();
+	game.draw();
 
 	enemy.move();
 	bullet.move();
+
+	for(let i = 0; i < bullet.arr.length; i++) {
+		for(let j = 0; j < enemy.arr.length; j++) {
+			if(bullet.touchesEnemy(i, j))
+			{
+				bullet.delete(i);
+				enemy.delete(j);
+			}
+		}
+	}
 
 	setTimeout(() => {
 		requestAnimationFrame(main);
